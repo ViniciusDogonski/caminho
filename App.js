60149e3514eb9847e071c3b0df32dd633ece7e59
vinicius <vinicius.dogonski@gmail.com>
2022-12-13 10:44:45 -0300

    test

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/screens/Login'
+import Profile from './src/screens/Profile';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Login/> */}
+      {/* <Profile /> */}
     </View>
   );
 }
@@ -14,7 +16,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
+
